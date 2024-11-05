5f51b351d141558bed3da9cef61c1706ba99c6a9
CPC2-2 <cmtorreno@gmail.com>
2024-11-05 19:33:44 +0100

    updated js and html

---
+++ src/Assets/js/index.js
@@ -0,0 +1,18 @@
+// start of the js
+let enviar = document.getElementById("enviar");
+let namebox = document.getElementById("name");
+let passwordbox = document.getElementById("password");
+let box_name_input = document.getElementById("box_name_input");
+let box_password_input = document.getElementById("box_password_input");
+
+//regular expresion for inputs check
+let Rexname = /^[A-Za-z]{2,30}$/;// regular expresion for name camp
+let Rexpassword = /^$/;// regular expresion for password camp
+
+namebox.addEventListener("input",(e)=>{
+    if (Rexname.test(namebox.value) === true){
+        namebox.style.border = "1px solid #17fa40";
+    }else{
+        namebox.style.border = "1px solid red";
+    }
+});
