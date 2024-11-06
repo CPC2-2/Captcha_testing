82cc9e82c413897410a9a71b36a14eed976f02c2
CPC2-2 <cmtorreno@gmail.com>
2024-11-06 19:03:20 +0100

    updated 06/11/2024

--- src/Assets/js/index.js
+++ src/Assets/js/index.js
@@ -7,7 +7,7 @@ let box_password_input = document.getElementById("box_password_input");
 
 //regular expresion for inputs check
 let Rexname = /^[A-Za-z]{2,30}$/;// regular expresion for name camp
-let Rexpassword = /^$/;// regular expresion for password camp
+let Rexpassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;// regular expresion for password camp
 
 namebox.addEventListener("input",(e)=>{
     if (Rexname.test(namebox.value) === true){
@@ -16,3 +16,12 @@ namebox.addEventListener("input",(e)=>{
         namebox.style.border = "1px solid red";
     }
 });
+//campos contrasenya
+passwordbox.addEventListener("input",(e)=>{
+    if (Rexpassword.test(passwordbox.value) === true){
+        passwordbox.style.border = "1px solid #17fa40";
+    }else{
+        passwordbox.style.border = "1px solid red";
+    }
+});
+
