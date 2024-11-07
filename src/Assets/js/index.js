99196cb5684b98ee624428b3b76a4c0874a32a97
CPC2-2 <cmtorreno@gmail.com>
2024-11-07 15:52:13 +0100

    updated code 15:51

--- src/Assets/js/index.js
+++ src/Assets/js/index.js
@@ -5,6 +5,10 @@ let passwordbox = document.getElementById("password");
 let box_name_input = document.getElementById("box_name_input");
 let box_password_input = document.getElementById("box_password_input");
 let show_error = document.getElementById("show_error");
+let mostrar_captcha = document.getElementById("mostrar_captcha");
+let chatpcha_box = document.getElementById("chatpcha_box");
+let chatpcha_container = document.getElementById("chatpcha_container");
+let captcha =  document.getElementById("captcha");
 
 //regular expresion for inputs check
 let Rexname = /^[A-Za-z]{2,30}$/;// regular expresion for name camp
@@ -30,10 +34,35 @@ passwordbox.addEventListener("input", (e) => {
 enviar.addEventListener("click", (e) => {
     e.preventDefault();
     if (Rexname.test(namebox.value) === true && Rexpassword.test(passwordbox.value) === true) {
-        //once we validate the data we can go to the captcha;
-        
+        chatpcha_box.style.display = "flex";
+        chatpcha_container.style.display = "flex";
+        namebox.style.border = "1px solid gray";
+        passwordbox.style.border = "1px solid gray";
+    }else{
+        const wrongdata = document.createElement("h3");
+        wrongdata.textContent = "Los datos no son correctos o no hay datos";
+        show_error.innerHTML = wrongdata.textContent;
     }
-    const wrongdata = document.createElement("h3");
-    wrongdata.textContent = "Los datos no son correctos";
-    show_error.innerHTML = wrongdata.textContent;
 })
+
+//generator(mostrar_captcha);
+// if (captcha.value.length === 0){ // comprovamos si el captcha ha sido rellenado
+//     alert("Primero confirma acaba los siguientes datos");
+// }else{
+//     console.log("Comprobacion Correcta");
+// }
+
+//function generator 
+function generator(caja){
+    let chars = "1234567890abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKKLMNÑOPQRSTUVWXYZ!·$%&/()=ª123456789";
+    let charslegnth = 7;
+    let captcha = "";
+
+    //hacemos el bucle para rrecorrer y generar la cadena
+    for (let i = 0; i < charslegnth; i++){
+        let random = Math.floor(Math.random() * chars.length);
+        captcha += chars.charAt(random);
+    }
+    // una vez tenemos la cadena la presentamos por pantalla para verificar si es un humano
+    caja.innerHTML = captcha;
+}
